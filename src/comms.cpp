47a0ab7d13695b78bd5ec24d060db4f84f18b43f
Akshit748 <73868401+Akshit748@users.noreply.github.com>
2026-02-26 15:42:59 +0530

    Update comms.cpp

--- src/comms.cpp
+++ src/comms.cpp
@@ -1,1 +1,10 @@
+#include <Arduino.h>
 
+void sendObstacleAlert(){
+  Serial.println("Obstacle Alert Sent");
+}
+
+void sendEmergency(){
+  Serial.println("Emergency SMS Sent!");}
+
+//for now this only sends messages in the serial monitor, this file and its contents serve as a placeholder for future features
